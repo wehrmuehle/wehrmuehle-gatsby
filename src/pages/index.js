3e478bf1c0457b3555f2830cad969d9024ebb091
Marco Emmanuel Noto <marcoemmanuelnoto@gmail.com>
2022-05-10 10:12:19 +0200

    made home page invisible

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,7 @@
 import React from 'react'
-import HomePageMobile from '../components/HomePage/HomePageMobile'
-import {useBreakpoint} from 'gatsby-plugin-breakpoints';
-import HomePageDekstop from '../components/HomePage/HomePageDekstop';
 
 export default function IndexPage() {
-    const breakpoints = useBreakpoint();
-
-    return (
-      <>
-        {breakpoints.sm &&  <HomePageMobile/>}
-        {(breakpoints.l && !breakpoints.sm) && <HomePageDekstop/>}
-      </>
-    )
+  return (
+    <div>index</div>
+  )
 }
---
+++ src/pages/temphome.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import HomePageMobile from '../components/HomePage/HomePageMobile'
+import {useBreakpoint} from 'gatsby-plugin-breakpoints';
+import HomePageDekstop from '../components/HomePage/HomePageDekstop';
+
+export default function TempHome() {
+    const breakpoints = useBreakpoint();
+
+    return (
+      <>
+        {breakpoints.sm &&  <HomePageMobile/>}
+        {(breakpoints.l && !breakpoints.sm) && <HomePageDekstop/>}
+      </>
+    )
+}
